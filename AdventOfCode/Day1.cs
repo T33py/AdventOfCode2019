4c67cd74b23f1dfa39420820e4f85b633dae6aaf
tom <tom@specialminds.dk>
2019-12-02 13:05:02 +0100

    first 2 days

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+        static void Main(string[] args)
+        {
+            FuelCostSolver solver = new FuelCostSolver();
+            string input = File.ReadAllText("RocketModuleWeights.txt");
+            //string input = File.ReadAllText("RocketModuleWeightTests.txt");
+            string[] weightStrings = input.Split('\n');
+            List<int> weights = new List<int>();
+
+            foreach(string s in weightStrings)
+            {
+                weights.Add(int.Parse(s));
+            }
+
+            List<int> fuelCosts = new List<int>();
+
+            foreach(int i in weights)
+            {
+                int fuel = solver.TotalFuel(i);
+
+                fuelCosts.Add(fuel);
+                Console.WriteLine(fuel);
+            }
+
+            var fuelTotal = 0;
+
+            foreach(int f in fuelCosts)
+            {
+                fuelTotal += f;
+            }
+
+            Console.WriteLine("Total: " + fuelTotal);
+
+            Console.ReadLine();
+        }
+    }
+
+    class FuelCostSolver
+    {
+        public int TotalFuel(int weight)
+        {
+            //Console.WriteLine("weight: " + weight);
+            int fuel = 0;
+
+            Double temp = weight;
+
+            fuel = ((int)Math.Floor((temp / 3))) - 2;
+
+            if(fuel >= 0)
+            {
+                fuel += TotalFuel(fuel);
+            }
+            else
+            {
+                fuel = 0;
+            }
+
+            //Console.WriteLine("fuel: " + fuel);
+            return fuel;
+        }
+    }
+}
---
+++ AdventOfCode/Day2.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode
+{
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            IntcodeComputer comp = new IntcodeComputer();
+            string input = File.ReadAllText("IntCode.txt");
+            //string input = File.ReadAllText("IntCodeTests.txt");
+            string[] programs = input.Split('\n');
+
+            foreach(string p in programs)
+            {
+                Console.WriteLine(p);
+
+
+                for(int i = 0; i<100; i++)
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        try
+                        {
+                            var program = comp.Parse(p);
+                            program[1] = i;
+                            program[2] = j;
+                            var result = comp.Run(program);
+                            Console.WriteLine(i + " " + j + " " + result[0]);
+
+                            if (result[0] == 19690720)
+                            {
+                                comp.Print(program);
+                                return;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            // invalid input
+                        }
+                    
+                    }
+                }
+
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+        
+    }
+
+    class IntcodeComputer
+    {
+        public List<int> Run(string input)
+        {
+            List<int> program = Parse(input);
+
+            return Compute(program);
+        }
+
+        public List<int> Run(List<int> input)
+        {
+            return Compute(input);
+        }
+
+        List<int> Compute(List<int> program)
+        {
+            int pointer = 0;
+
+            // while not looking at the halting operation
+            while (program[pointer] != 99)
+            {
+                var opcode = program[pointer];
+                var a = program[pointer + 1];
+                var b = program[pointer + 2];
+                var storeTo = program[pointer + 3];
+
+                var result = compute(opcode, program[a], program[b]);
+
+                program[storeTo] = result;
+
+                pointer = pointer + 4;
+                //Print(program);
+            }
+
+            return program;
+        }
+
+        int compute(int opcode, int var1, int var2)
+        {
+            int result = -1;
+            if(opcode == 1)
+            {
+                result = var1 + var2;
+            }
+            else if(opcode == 2)
+            {
+                result = var1 * var2;
+            }
+
+            return result;
+        }
+
+        public List<int> Parse(string input)
+        {
+            List<int> program = new List<int>();
+
+            string[] temp = input.Split(',');
+
+            foreach(string s in temp)
+            {
+                program.Add(int.Parse(s));
+            }
+
+            return program;
+        }
+
+        public void Print(List<int> program)
+        {
+            Console.Write("{ ");
+            for (int i = 0; i < program.Count - 1; i++)
+            {
+                Console.Write(program[i] + ", ");
+            }
+            Console.Write(program[program.Count - 1]);
+            Console.WriteLine(" }");
+        }
+    }
+}
